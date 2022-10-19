05f6c9e946efdc0c474171032d468e65c6a33f5e
Alan Xu <61564861+alanxu323@users.noreply.github.com>
2022-10-18 20:06:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,13 +15,6 @@ def process_msg(msg):
 	if msg.lower() == "Hi".lower():
 		response = "Welcome to the stock market bot!"
 		response += "Type sym:<stock_symbol> to know the price of the stock"
-	elif msg == "小狐狸":
-		response = "深度睿智！"
-	elif msg == "主人宝贝":
-		response = "最聪明的人，拍拍聪明可爱大脑袋"
-	elif msg == "世界上最可爱的人是谁":
-		response = "是Guniu呜们!"
-	
 	elif 'sym:' in msg:
 		data = msg.split(":")
 		stock_symbol = data[1]
@@ -47,8 +40,3 @@ def webhook():
 	response = process_msg(msg)
 	send_msg(response, sender)
 	return "OK", 200
-
-#Account ID(TWILIO_ACCOUNT, TWILIO_TOKEN)
-#AC62ff9d67e3160c8fcb96f6a993aa3391
-#Token
-#2c95e1bd47890942c2aec2264ceb92cc
