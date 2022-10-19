464070a02d26fcdff1da3089e982fb144ed065f6
Alan Xu <61564861+alanxu323@users.noreply.github.com>
2022-10-18 21:14:09 -0400

    Update marketstack.py

--- marketstack.py
+++ marketstack.py
@@ -11,16 +11,12 @@ def get_stock_price(stock_symbol):
 	params = {
 		'access_key': api_key
 	}
-	#end_point = ''.join([base_url,stock_symbol, "/intraday/latest"])
-	#http://api.marketstack.com/v1/tickers/aapl/intraday
-	#http://api.marketstack.com/v1/tickers/aapl/intraday/latest
-	#print(end_point)
+	
 	api_result = requests.get('http://api.marketstack.com/v1/tickers/aapl/intraday/latest', params)
 	print(api_result)
 	
 	json_result = json.loads(api_result.text)
-	#return json_result
-	#return end_point
+
 	return{"highest": json_result["high"],
 		"lowest": json_result["low"],
 		"close": json_result["close"]}
